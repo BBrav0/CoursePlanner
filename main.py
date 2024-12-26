952f09a40071f0879308e2957eb54dcf3b9178d5
Ben Bravo <bendc312@gmail.com>
2024-12-25 19:20:55 -0800

    add button putting in frames

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from tkinter import ttk
 # WINDOW CLEAR METHOD
 def clear_window():
     for widget in root.winfo_children():
+        if (isinstance(widget, Menu)):
+            continue
         widget.destroy()
 
 # MISC METHOD
@@ -57,15 +59,64 @@ def start_win():
     btn.pack(side=TOP, padx=10, pady=10)
 
 # ADD COURSE BUTTON
-def add():
-    popup = Tk()
+def add(xx, yy):
+    global popup
+    popup = Toplevel(root)
     popup.title("Add Course")
-    popup.geometry('800x800')
+    popup.geometry('600x400')
 
     popup.lift()
     popup.focus_force()
 
-    root.mainloop()
+    nex = Label(popup, text="Course Code", borderwidth=2, relief="solid")
+    nex.place(x=0, y=0)
+
+    nex = Label(popup, text="Course Title", borderwidth=2, relief="solid")
+    nex.place(x=0, y=50)
+
+    nex = Label(popup, text="Credits", borderwidth=2, relief="solid")
+    nex.place(x=0, y=100)
+
+    nex = Label(popup, text="Grade", borderwidth=2, relief="solid")
+    nex.place(x=0, y=150)
+
+    grades = ["","A","A-","B+","B","B-","C+","C","C-","F"]
+
+    variable = StringVar(root)
+    variable.set(grades[0])
+
+    grade = OptionMenu(popup, variable, *grades)
+    grade.place(x=100, y=150)
+
+    global code
+    code = Entry(popup, width=15, borderwidth=2, relief="solid")
+    code.place(x=100, y=0)
+
+    global title
+    title = Entry(popup, width=20, borderwidth=2, relief="solid")
+    title.place(x=100, y=50)
+
+    global cred
+    cred = Entry(popup, width=5, borderwidth=2, relief="solid")
+    cred.place(x=100, y=100)
+
+    btn = Button(popup, text="Add", fg="black", command=lambda: added(xx, yy), borderwidth=2, relief="solid")
+    btn.pack(side=BOTTOM, pady=50)
+
+
+# COURSE ADDED BUTTON
+def added(xx, yy):
+    variable.get()
+    title.get()
+    cred.get()
+    code.get()
+
+    frame = Frame(root, borderwidth=3, relief="raised", width=180, height=50, bg="lightgrey")
+    frame.place(x=xx+10, y=yy)
+
+    popup.destroy()
+
+
 
 #
 # COURSE PAGE BUILD
@@ -80,8 +131,12 @@ def course_page(year):
         nex = Label(root, text=cur +" "+str(year), borderwidth=2, relief="solid")
         nex.place(x=j, y=0)
 
-        btn = Button(root, text="+", fg="black", command=add, borderwidth=2, relief="solid")
-        btn.place(x=j, y=30)
+        frame = Frame(root, borderwidth=5, relief="sunken", width=200, height=500)
+        frame.place(x=j, y=20)
+
+        btn = Button(root, text="+", fg="black", command=lambda x=j: add(x, 30), borderwidth=2, relief="solid")
+        btn.place(x=j+70, y=30)
+
 
         if(cur == "Fall"):
             j+=210
@@ -117,4 +172,3 @@ root.lift()
 root.focus_force()
 
 root.mainloop()
-
